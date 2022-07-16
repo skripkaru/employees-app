fa7b5a15d042419f70e6c8effd7c3b83cbda91c6
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2022-07-16 17:13:41 +0300

    init

---
+++ src/components/app/App.tsx
@@ -0,0 +1,25 @@
+import AppInfo from '../appInfo/AppInfo';
+import SearchPanel from '../searchPanel/SearchPanel';
+import AppFilter from '../appFilter/AppFilter';
+import EmployeesList from '../employeesList/EmployeesList';
+import EmployeesAddForm from '../employeesAddForm/EmployeesAddForm';
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/employeesList/EmployeesList.tsx
@@ -0,0 +1,15 @@
+import EmployeesListItem from "../employeesListItem/EmployeesListItem";
+
+import './styles.css';
+
+const EmployeesList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+        </ul>
+    )
+}
+
+export default EmployeesList;
